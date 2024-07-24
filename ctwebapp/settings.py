5bfbed153f1210ddf2abdc5b3a3e3628594a6768
Sahil Bharadwaj <119106087+Sahil0215@users.noreply.github.com>
2024-07-24 22:37:50 +0530

    Update settings.py
    commit changes in settings

--- ctwebapp/settings.py
+++ ctwebapp/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7g!z1bf#(=4n4h$y8#+y62caos#i8)5_-td-#(0fqyu21b5z*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pythondemowebappsctds.azurewebsites.net']
 
 
 # Application definition
